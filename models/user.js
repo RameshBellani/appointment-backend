399753b3a5ba30ced16815fa3fa72bdf9a8c7846
RameshBellani <136491880+RameshBellani@users.noreply.github.com>
2024-06-28 17:43:30 +0530

    appointment mysql

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const connection = require('../config/db');
+const bcrypt = require('bcryptjs');
+
+const User = {
+  create: (username, hashedPassword) => {
+    return new Promise((resolve, reject) => {
+      const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+      connection.query(query, [username, hashedPassword], (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    });
+  },
+  findByUsername: (username) => {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT * FROM users WHERE username = ?';
+      connection.query(query, [username], (err, results) => {
+        if (err) reject(err);
+        resolve(results[0]);
+      });
+    });
+  }
+};
+
+module.exports = User;
